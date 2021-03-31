527a89ce40aa0ca2b33f15d83d0aaaad5f85ec6d
Hithardh <hithardh14122000@gmail.com>
2021-03-31 10:55:00 +0530

    Basic first pattern

---
+++ pattern1.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	int N;
+	cout<<"Enter the N value:";
+	cin>>N;
+	for(int i=0;i<N;i++){
+		for(int i=0;i<N;i++){
+			cout<<"* ";
+		}
+		cout<<endl;
+	}
+}
